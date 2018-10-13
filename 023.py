757b9c2f650eb5281bb2ce2ee703f70b83588149
Angelo768 <angeliano12@hotmail.com>
2018-10-13 00:14:35 -0300

    023.py

---
+++ 023.py
@@ -0,0 +1,9 @@
+num = int(input("Digite um inteiro: "))
+u = num%10
+d = (num%100)//10
+c = (num%1000)//100
+m = (num%10000)//1000
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
