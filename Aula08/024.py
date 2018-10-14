38b6fb0cc76f6c4c9355ef02ab9ee4effa6fbc26
Angelo768 <angeliano12@hotmail.com>
2018-10-14 01:04:57 -0300

    Aula09

---
+++ Aula08/024.py
@@ -0,0 +1,5 @@
+cid = input("Nome da cidade: ").lower()
+if cid.startswith('santo'):
+    print('O primeiro nome de sua cidade é Santo')
+else:
+    print('O nome da sua cidade não começa com Santo')
